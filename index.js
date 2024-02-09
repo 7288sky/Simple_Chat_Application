7aa25c7affb8ca0b8d90b9536244d990160df67f
7288sky <123364227+7288sky@users.noreply.github.com>
2024-02-09 14:36:31 +0530

    Check Socket.io by connection event

---
+++ index.js
@@ -0,0 +1,39 @@
+import express from "express"
+const app=express();
+
+import http from "http"
+
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+const expressServer=http.createServer(app)
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+
+import { Server } from "socket.io";
+import { Socket } from "dgram";
+
+const io=new Server(expressServer)
+
+
+
+
+
+io.on('connection',(Socket)=>{
+    console.log("New user connected")
+})
+
+
+
+
+
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname+'/index.html')
+})
+
+
+
+expressServer.listen(3000,(req,res)=>{
+    console.log("server is running on port 3000")
+})
